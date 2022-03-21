052e01c47b67e9704da99c848ef4edb0c6d6df24
Omar AbdElwahhab <omarabdelwahhabkishk2000@gmail.com>
2022-03-21 22:05:56 +0200

    Update mail.php

--- app/lib/mail.php
+++ app/lib/mail.php
@@ -16,6 +16,8 @@ $mail = new PHPMailer();
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = '';                     //SMTP username
+$mail->Password   = '';                               //SMTP password
 $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
 $mail->Port       = 465;
 
